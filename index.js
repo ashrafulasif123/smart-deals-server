c7fbb072ded704a205ed30190f31d0dbcfa3da1d
Afridul369 <afridul.dev@gmail.com>
2026-07-01 00:41:47 +0600

    smart server

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+/** client থেকে Server Side অনুমোদন পাওয়ার জন্য CORS Middleware for Express */
+const cors = require("cors");
+app.use(cors());
+/** Client Side থেকে Data JSON stringify হয়ে আসার সময় JSON Parse করতে হয়  */
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://smartdbUser:3VoBHDBqKYJ6ZOwP@cluster0.mrg0zof.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+/* Express Route */
+app.get("/", (req, res) => {
+  res.send("Smart Deals is running");
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("smartdbUser");
+    const productsCollection = db.collection("products");
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          price: updatedProduct.price,
+        },
+      };
+      const result = await productsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Smart Deals app listening on port ${port}`);
+});
