1ec6bd12f5f9470831743939ad658bb202688926
ashrafulasif123 <cseasifiiuc9@gmail.com>
2026-07-07 17:36:20 +0600

    dgdd

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     await client.connect();
     const db = client.db("smartdbUser");
     const productsCollection = db.collection("products");
+    const bidsCollection = db.collection("bids");
 
     app.get("/products", async (req, res) => {
       /* const projectFields = { title: 1, price_min: 1, price_max: 1, image: 1 };
@@ -85,6 +86,39 @@ async function run() {
       const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
+
+    // bids related api
+    app.get("/bids", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.buyer_email = email;
+      }
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/bids/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { buyer_email: email };
+      const result = await bidsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/bids", async (req, res) => {
+      const newBid = req.body;
+      const result = await bidsCollection.insertOne(newBid);
+      res.send(result);
+    });
+
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
